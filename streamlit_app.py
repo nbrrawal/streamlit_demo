7dde0bc862b840dbb988175f442b724efbb3f253
BlueDragon <nbrrawal@rediffmail.com>
2024-01-10 23:29:19 +0530

    Updating time zone and pushing to git repo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,8 @@
 import streamlit as st
 import time
-
-
-# NOTE: This must be the first command in your app, and must be set only once
-st.set_page_config(layout="wide")
-
-
-# Function to get the current time
-user_count = 0
+import pytz
+from datetime import datetime, timezone
+# from tzlocal import get_localzone
 
 
 def get_current_time():
@@ -16,17 +11,20 @@ def get_current_time():
 # UI elements
 # user_count+1
 
-
-user_count = user_count+1
+# user_count = user_count+1
 
 # Streamlit app
 
+
 st.title(" Real Time clock Demo 1")
 st.divider()
-st.write("Delhi time Now : ")
+st.write("Delhi Time Now : ")
 time_container = st.empty()
 
 while True:
-    current_time = get_current_time()
-    time_container.write(f"Current Time: {current_time}")
+    utc_dt = datetime.now(timezone.utc)
+    IND = pytz.timezone("Asia/Kolkata")
+    time_container.write(f" {utc_dt.astimezone(IND).strftime('%A %d. %B %Y %H:%M:%S')}")
     time.sleep(1)
+
+#    current_time = time.strftime("%H:%M:%S")
