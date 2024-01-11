da9eb7bc54eedbc2ef1315e1041969838397c7b6
BlueDragon <nbrrawal@rediffmail.com>
2024-01-12 00:12:33 +0530

    Update streamlit_app.py
    full version with all time zones updated with experimental run option

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,37 +2,38 @@ import streamlit as st
 import time
 import pytz
 from datetime import datetime, timezone
-# from tzlocal import get_localzone
 
-
-def get_current_time():
-    return time.strftime("%H:%M:%S")
-
-# UI elements
-# user_count+1
-
-# user_count = user_count+1
-
-# Streamlit app
-
-
-st.title(" Real Time clock Demo 1")
-st.divider()
-st.write("Delhi Time Now : ")
-time_container = st.empty()
+utc_dt = datetime.now(timezone.utc)
 nl = '\n'
 
-with time_container.container():
-    while True:
-        utc_dt = datetime.now(timezone.utc)
-        IND = pytz.timezone("Asia/Kolkata")
-        FRA = pytz.timezone("Europe/Paris")
-        USA = pytz.timezone("US/Pacific")   # Las Vegas
-        RUS = pytz.timezone("Europe/Moscow")
-        SGP = pytz.timezone("Asia/Singapore")
-        AUS = pytz.timezone("Australia/Sydney")
-        time_container.write(f" { utc_dt.astimezone(IND).strftime('%A %d. %B %Y %H:%M:%S')}" + nl.join("\n") + f" { utc_dt.astimezone(FRA).strftime('%A %d. %B %Y %H:%M:%S') }")
-        time.sleep(1)
+IND = pytz.timezone("Asia/Kolkata")
+FRA = pytz.timezone("Europe/Paris")
+USA = pytz.timezone("US/Pacific")   # Las Vegas
+RUS = pytz.timezone("Europe/Moscow")
+SGP = pytz.timezone("Asia/Singapore")
+AUS = pytz.timezone("Australia/Sydney")
 
-#    current_time = time.strftime("%H:%M:%S")
-#+ nl +  + nl+ utc_dt.astimezone(USA).strftime('%A %d. %B %Y %H:%M:%S')+ nl+ utc_dt.astimezone(RUS).strftime('%A %d. %B %Y %H:%M:%S')+nl+ utc_dt.astimezone(SGP).strftime('%A %d. %B %Y %H:%M:%S') + nl+utc_dt.astimezone(AUS).strftime('%A %d. %B %Y %H:%M:%S')
+# Display the current time
+st.title('World Clocks ')
+current_time = datetime.now().strftime("%H:%M:%S")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.title("Delhi")
+    st.write(utc_dt.astimezone(IND).strftime('%A %d. %B %Y \n %H:%M:%S'))
+    st.title("RUS")
+    st.write(f"  { utc_dt.astimezone(RUS).strftime('%A %d. %B %Y %H:%M:%S')}")
+with col2:
+    st.title("France")
+    st.write(utc_dt.astimezone(FRA).strftime('%A %d. %B %Y \n %H:%M:%S'))
+    st.title("SGP")
+    st.write(f"  {  utc_dt.astimezone(SGP).strftime('%A %d. %B %Y %H:%M:%S')}")
+with col3:
+    st.title("USA")
+    st.write(utc_dt.astimezone(USA).strftime('%A %d. %B %Y \n %H:%M:%S'))
+    st.title("AUS")
+    st.write(f"  {utc_dt.astimezone(AUS).strftime('%A %d. %B %Y %H:%M:%S')}")
+
+# Sleep for 5 seconds before rerunning
+time.sleep(1)
+st.experimental_rerun()
