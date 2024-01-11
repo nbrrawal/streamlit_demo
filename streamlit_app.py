773ca8f22b85f34bda27af6995a98531b40c2201
BlueDragon <nbrrawal@rediffmail.com>
2024-01-12 00:09:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,19 @@ st.title(" Real Time clock Demo 1")
 st.divider()
 st.write("Delhi Time Now : ")
 time_container = st.empty()
+nl = '\n'
 
-while True:
-    utc_dt = datetime.now(timezone.utc)
-    IND = pytz.timezone("Asia/Kolkata")
-    time_container.write(f" {utc_dt.astimezone(IND).strftime('%A %d. %B %Y %H:%M:%S')}")
-    time.sleep(1)
+with time_container.container():
+    while True:
+        utc_dt = datetime.now(timezone.utc)
+        IND = pytz.timezone("Asia/Kolkata")
+        FRA = pytz.timezone("Europe/Paris")
+        USA = pytz.timezone("US/Pacific")   # Las Vegas
+        RUS = pytz.timezone("Europe/Moscow")
+        SGP = pytz.timezone("Asia/Singapore")
+        AUS = pytz.timezone("Australia/Sydney")
+        time_container.write(f" { utc_dt.astimezone(IND).strftime('%A %d. %B %Y %H:%M:%S')}" + nl.join("\n") + f" { utc_dt.astimezone(FRA).strftime('%A %d. %B %Y %H:%M:%S') }")
+        time.sleep(1)
 
 #    current_time = time.strftime("%H:%M:%S")
+#+ nl +  + nl+ utc_dt.astimezone(USA).strftime('%A %d. %B %Y %H:%M:%S')+ nl+ utc_dt.astimezone(RUS).strftime('%A %d. %B %Y %H:%M:%S')+nl+ utc_dt.astimezone(SGP).strftime('%A %d. %B %Y %H:%M:%S') + nl+utc_dt.astimezone(AUS).strftime('%A %d. %B %Y %H:%M:%S')
